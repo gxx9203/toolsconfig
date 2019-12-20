0407b10f5c2e28761b55082d8c74d8b700081b1c
gxx9203 <gxx9203@163.com>
2019-12-20 22:24:01 +0800

    Create ubuntu_install.sh

---
+++ ubuntu_install.sh
@@ -0,0 +1,7 @@
+sudo apt-get install -y tree vim tmux
+sudo apt-get install -y repo git
+sudo apt-get install -y python3
+sudo apt-get install -y python3-smbc
+sudo apt-get install -y meld
+sudo apt-get install -y audacity
+sudo apt-get install -y adb fastboot
