815fa661ad65a1451c838d3e9611241fa4b9594f
gxx9203 <gxx9203@163.com>
2019-12-25 12:49:29 +0800

    Update config_computer.sh

--- config_computer.sh
+++ config_computer.sh
@@ -4,9 +4,11 @@ echo "config pc enviroment"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ~/.vimrc ~/.vimrc_back
 cp ~/.tmux.conf ~/.tmux.conf_back
+cp ~/.bash_profile ~/.bash_profile_back
 
-cp ./vimrc ~/.vimrc
-cp ./tmux.conf ~/.tmux.conf
+cp ./.vimrc ~/.vimrc
+cp ./.tmux.conf ~/.tmux.conf
+cp ./.bash_profile ~/.bash_profile
 
 
 echo "please git enviroment"
